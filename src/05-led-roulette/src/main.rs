e6bb98ff3bddcbd6a8f0dfd8e55a19c6e6c9a79d
Walter Chang <weihsiu@gmail.com>
2021-02-15 01:40:31 +0800

    5 led done

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,20 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::*;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
-
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
+    let period = 50u16;
+    let mut count = 0;
+    loop {
+        for i in 0..8 {
+            let j = (i + 1) % 8;
+            leds[j].on();
+            delay.delay_ms(period);
+            leds[i].off();
+            delay.delay_ms(period);
+        }
+    }
 }
