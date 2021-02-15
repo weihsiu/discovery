a5ad0b8649fa8f5d6da75efbd284214a18176bbd
Walter Chang <weihsiu@gmail.com>
2021-02-16 02:28:23 +0800

    7 done

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,29 +1,35 @@
 #![no_main]
 #![no_std]
 
+use core::ptr;
+
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let (mut itm, gpioe) = aux7::init();
 
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
+    iprintln!(&mut itm.stim[0], "done {}", gpioe.odr.read().odr9().bit());
 
-        // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
+    // unsafe {
+    //     // A magic address!
+    //     const GPIOE_BSRR: u32 = 0x48001018;
 
-        // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
+    //     // Turn on the "North" LED (red)
+    //     *(GPIOE_BSRR as *mut u32) = 1 << 9;
 
-        // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+    //     // Turn on the "East" LED (green)
+    //     *(GPIOE_BSRR as *mut u32) = 1 << 11;
 
-        // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
-    }
+    //     // Turn off the "North" LED
+    //     *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+
+    //     // Turn off the "East" LED
+    //     *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+    // }
 
     loop {}
 }
